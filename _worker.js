fec7631aac650c3735cee7de2e91b706a983fa93
PoemMistyMoon <167159446+PoemMistyMoon@users.noreply.github.com>
2024-04-28 14:39:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -191,7 +191,7 @@ else
   exit 1
 fi
 BASE64_TEXT=$(head -n 65 $FILENAME | base64 -w 0)
-curl -k "https://$DOMAIN/$FILENAME?token=$TOKEN&b64=$BASE64_TEXT"
+curl -m 30 -k "https://$DOMAIN/$FILENAME?token=$TOKEN&b64=$BASE64_TEXT"
 echo "更新数据完成"
 `
 }
